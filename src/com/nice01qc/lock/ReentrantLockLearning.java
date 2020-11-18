3fe03dd5983f5fc925a9e02913e358e7c21041ae
Li Jinhao <nice01qc@gmail.com>
2020-11-19 00:32:16 +0800

    init

--- src/com/nice01qc/lock/ReentrantLockLearning.java
+++ src/com/nice01qc/lock/ReentrantLockLearning.java
@@ -13,19 +13,32 @@ public class ReentrantLockLearning {
         lock.lock();
         Condition condition = lock.newCondition();
 
-        new Thread(() -> {
-            lock.lock();
-            System.out.println(123);
-            try {
-                Thread.sleep(1000*1);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            condition.signal(); // 将主线程 Node 的 waitStatus => 0
-            lock.unlock(); // unPark 下个Node 值 大于 0 的, 然后主线程被 unpark
-        }).start();
+        for (int i = 0; i < 3; i++) {
+            int finalI = i;
+            new Thread(() -> {
+                lock.lock();
+                try {
+                    Thread.sleep(1000 * 2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if (finalI != 2){
+                    try {
+                        condition.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }else {
+                    condition.signalAll();
+                }
+                System.out.println(finalI);
 
-        condition.await(1000, TimeUnit.MINUTES); // waitStatus => -2 CONDITION， 然后被park
+//                condition.signal(); // 将主线程 Node 的 waitStatus => 0
+                lock.unlock(); // unPark 下个Node 值 大于 0 的, 然后主线程被 unpark
+            }).start();
+        }
+
+        condition.await(2, TimeUnit.SECONDS); // waitStatus => -2 CONDITION， 然后被park
         lock.unlock();
 
         ExecutorService executorService = Executors.newCachedThreadPool();

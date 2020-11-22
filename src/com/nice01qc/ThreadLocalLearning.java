653cf70fc9cdc1bc47e9fec43ac7a7eb8f14e11f
Li Jinhao <nice01qc@gmail.com>
2020-11-23 00:15:34 +0800

    ThreadLocal 学习

--- src/com/nice01qc/ThreadLocalLearning.java
+++ src/com/nice01qc/ThreadLocalLearning.java
@@ -12,7 +12,19 @@ public class ThreadLocalLearning {
     };
     public static void main(String[] args) {
 
-        System.out.println(threadLocal.get());
+//        System.out.println(threadLocal.get());
+//
+//        System.out.println(Integer.toBinaryString(-128));
+//        System.out.println(Integer.toBinaryString(128));
+//        System.out.println(Integer.toBinaryString(127));
+        String[] strings = new String[]{" ", "  ", "   ", "    ", "     ", "      ", "       ", "            ", "            ", "              ","              ", "              ","        "};
+        for (int i = 0; i < 100000000; i++){
+            String out1 = strings[Long.valueOf(Math.round(Math.random() * 1000)).intValue() % strings.length];
+            String out2 = strings[Long.valueOf(Math.round(Math.random() * 1000)).intValue() % strings.length];
+            String out3 = strings[Long.valueOf(Math.round(Math.random() * 1000)).intValue() % strings.length];
+            System.out.println(out1 + "I" + out3 + "love" + out2 + "you");
+        }
+
 
     }
 }
